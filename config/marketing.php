e3788a774e1101e84959178acd18b2eca402b6ab
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2022-03-27 17:48:33 +0200

    Update home page employers logo cloud

---
+++ config/marketing.php
@@ -0,0 +1,80 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    |
+    */
+
+    'employers_showcase' => [
+        'Aegean Airlines',
+        'Aerosotravia',
+        // 'Air Austral',
+        // 'Air Caraibes',
+        // 'Air Corsica',
+        // 'Air France Hop',
+        'Air France',
+        'Air Macau',
+        // 'Air Mauritius',
+        // 'airBaltic',
+        'AirCalin',
+        // 'ASL Airlines France',
+        // 'ASL Airlines Switzerland',
+        'Astonfly',
+        // 'Austrian Airlines',
+        'Babcock International',
+        // 'Blue Islands',
+        // 'British Airways',
+        'Cargolux',
+        'Cathay Pacific',
+        // 'Cebu Pacific',
+        // 'Chalair Aviation',
+        // 'CityJet',
+        'Cretan Eagle Aviation',
+        'DHL',
+        'easyJet',
+        // 'ENAC',
+        'Etihad Airways',
+        // 'Etihad Regional',
+        // 'Flybe',
+        // 'Flydubai',
+        'Garuda Indonesia',
+        // 'GlobalJet',
+        // 'GlobeAir',
+        // 'Hong Kong Airlines',
+        // 'Intercaribbean Airways',
+        // 'Jetstar',
+        // 'La Compagnie',
+        'LOT Polish Airlines',
+        'Lufthansa Cityline',
+        'Luxair',
+        // 'LuxAviation',
+        // 'Norwegian',
+        // 'Oyonnair',
+        'PixAir Survey',
+        // 'PrivatAir',
+        // 'Qantas',
+        // 'Qatar Airways',
+        'Ryanair',
+        'St Barth Commuter',
+        // 'St Barth Executive',
+        // 'Susi Air',
+        // 'Swiss',
+        'Transavia',
+        // 'TUI Fly',
+        // 'VallJet',
+        'VistaJet',
+        'Volotea',
+        'Vueling',
+        'West Atlantic',
+        // 'Wizz Air',
+    ],
+
+];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -154,22 +154,16 @@
             <p class="text-center text-sm font-semibold uppercase text-gray-500 tracking-wide">
                 Plus de 100 exploitants nous font confiance
             </p>
-            <div class="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
-                <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-                    <img class="h-12" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple">
-                </div>
-                <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-                    <img class="h-12" src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Mirage">
-                </div>
-                <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-                    <img class="h-12" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit">
-                </div>
-                <div class="col-span-1 flex justify-center md:col-span-2 md:col-start-2 lg:col-span-1">
-                    <img class="h-12" src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg" alt="Transistor">
-                </div>
-                <div class="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">
-                    <img class="h-12" src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg" alt="Workcation">
-                </div>
+            <div class="-mx-4 mt-6 flex flex-wrap justify-center">
+                @foreach(config('marketing.employers_showcase') as $employer)
+                    <div class="flex-none flex p-4 justify-center w-1/3 md:w-1/4 lg:w-1/5">
+                        <img
+                            class="h-12"
+                            src="{{ asset('media/logos/' . Str::slug($employer) . '-logo-gray-400.svg') }}"
+                            alt="{{ $employer }}"
+                        >
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
