8f64b3d7ebe3ddbb2ed7196dbe850a8c683191f3
Hugo Clarke-Wing <7689302+clarkewing@users.noreply.github.com>
2025-08-06 19:19:25 +0200

    Remove class safelist

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -18,19 +18,46 @@
     };
 @endphp
 
-<div {{ $attributes->merge(['class' => "rounded-md bg-$color-50 p-4"]) }}>
+<div {{ $attributes->class([
+    'rounded-md p-4',
+    'bg-blue-50' => $color === 'blue',
+    'bg-green-50' => $color === 'green',
+    'bg-yellow-50' => $color === 'yellow',
+    'bg-red-50' => $color === 'red',
+]) }}>
     <div class="flex">
         <div class="shrink-0">
-            <x-dynamic-component :component="$icon" class="h-5 w-5 text-{{ $color }}-400" />
+            <x-dynamic-component
+                :component="$icon"
+                @class([
+                    'h-5 w-5',
+                    'text-blue-400' => $color === 'blue',
+                    'text-green-400' => $color === 'green',
+                    'text-yellow-400' => $color === 'yellow',
+                    'text-red-400' => $color === 'red',
+                ])
+            />
         </div>
         <div class="ml-3 flex-1 md:flex md:justify-between">
             <div class="space-y-2">
                 @isset($header)
-                    <h3 class="text-sm font-medium text-{{ $color }}-800">
+                    <h3 @class([
+                        'text-sm font-medium',
+                        'text-blue-800' => $color === 'blue',
+                        'text-green-800' => $color === 'green',
+                        'text-yellow-800' => $color === 'yellow',
+                        'text-red-800' => $color === 'red',
+                    ])>
                         {{ $header }}
                     </h3>
                 @endisset
-                <p class="text-sm text-{{ $color }}-700">
+                <p @class([
+                    'text-sm',
+                    'text-blue-700' => $color === 'blue',
+                    'text-green-700' => $color === 'green',
+                    'text-yellow-700' => $color === 'yellow',
+                    'text-red-700' => $color === 'red',
+                ])>
                     {{ $slot }}
                 </p>
             </div>
